db5d489df7c74219d58eefded20fc3633d57b58e
psid <jverhaeghe.dev@gmail.com>
2015-03-05 15:51:14 +0100

    Possibilité de compresser les js et les css amélioration du gulpfile

--- app/pages/accueil/accueil.js
+++ app/pages/accueil/accueil.js
@@ -4,7 +4,7 @@
     angular.module('jverhaeghePortfolio.accueil')
         .controller('Accueil', Accueil);
 
-
+    Accueil.$inject = ["$scope","$rootScope"];
     function Accueil($scope,$rootScope){
         $scope.pageClass = "accueil";
         $rootScope.menuIsOpen = "";
